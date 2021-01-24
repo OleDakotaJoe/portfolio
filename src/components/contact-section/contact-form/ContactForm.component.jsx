61cc307982dd616642107529a7e619b707d8c58b
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-01-24 04:25:27 -0600

    Added floating form labels

--- src/components/contact-section/contact-form/ContactForm.component.jsx
+++ src/components/contact-section/contact-form/ContactForm.component.jsx
@@ -19,32 +19,71 @@ const ContactForm = () => {
 		);
 	};
 
+	const animateLabel = (event) => {
+		const label = event.target.labels[0];
+		const classList = label.classList;
+		if (event.target.value !== "") {
+			if (!classList.contains("section-contact__contact-form--label-active")) {
+				classList.add("section-contact__contact-form--label-active");
+			}
+		} else {
+			classList.remove("section-contact__contact-form--label-active");
+		}
+	};
+
 	return (
 		<form className="section-contact__contact-form" onSubmit={sendEmail}>
 			<h4 className="section-contact__contact-form--heading">
 				Want to get connected?
 			</h4>
-			<input
-				name="from_name"
-				id="name"
-				type="text"
-				placeholder="Name"
-				required="true"
-				className="section-contact__contact-form--input "
-			/>
-			<input
-				name="reply_to"
-				id="email"
-				type="email"
-				required="true"
-				placeholder="Email Address"
-				className="section-contact__contact-form--input "
-			/>
+			<div className="section-contact__contact-form--upper-container">
+				<div className="section-contact__contact-form--upper-container-row section-contact__contact-form--upper-container-row-1">
+					<div className="section-contact__contact-form--label-box section-contact__contact-form--label-box--name">
+						<label
+							htmlFor="name"
+							className="section-contact__contact-form--label "
+						>
+							Name
+						</label>
+					</div>
+					<div className="section-contact__contact-form--label-box">
+						<label
+							htmlFor="email"
+							className="section-contact__contact-form--label"
+						>
+							Email Address
+						</label>
+					</div>
+				</div>
+				<div className="section-contact__contact-form--upper-container-row section-contact__contact-form--upper-container-row-2">
+					<input
+						name="from_name"
+						id="name"
+						onChange={animateLabel}
+						type="text"
+						placeholder="Name"
+						required={true}
+						className="section-contact__contact-form--input section-contact__contact-form--input--name"
+					/>
+					<input
+						name="reply_to"
+						id="email"
+						onChange={animateLabel}
+						type="email"
+						required={true}
+						placeholder="Email Address"
+						className="section-contact__contact-form--input "
+					/>
+				</div>
+			</div>
+			<label htmlFor="message" className="section-contact__contact-form--label">
+				Message
+			</label>
 			<textarea
 				name="message"
-				placeholder="Message"
-				required="true"
 				id="message"
+				onChange={animateLabel}
+				placeholder="Message"
 				className="section-contact__contact-form--input section-contact__contact-form--message"
 			></textarea>
 			<CustomButton type="submit">SUBMIT</CustomButton>
