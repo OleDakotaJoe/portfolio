d2ba9fd96e2950e587d2f8cfb98b1e94940f738b
OleDakotaJoe <stevendakotasheaves@gmail.com>
2021-01-27 03:41:23 -0600

    Added footer section

--- src/components/footer/Footer.container.jsx
+++ src/components/footer/Footer.container.jsx
@@ -16,6 +16,36 @@ const Footer = () => {
 					/>
 				</AnchorLink>
 			</div>
+			<div className="section-footer__social">
+				<CircleButton
+					onClick={() =>
+						(window.location.href = "https://github.com/oledakotajoe")
+					}
+					icon="github2.png"
+					altText="View Github"
+					animated
+					inverted
+				/>
+				<CircleButton
+					onClick={() =>
+						(window.location.href =
+							"https://www.linkedin.com/in/steven-sheaves/")
+					}
+					icon="linkedin.png"
+					altText="Visit LinkedIn"
+					animated
+					inverted
+				/>
+				<CircleButton
+					onClick={() =>
+						(window.location.href = "mailto:steve@stevensheaves.me")
+					}
+					icon="email.png"
+					altText="Email Me"
+					animated
+					inverted
+				/>
+			</div>
 		</footer>
 	);
 };
--- src/components/reusable/circle-button/CircleButton.component.jsx
+++ src/components/reusable/circle-button/CircleButton.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./circle-button.styles.scss";
 
-const CircleButton = ({ onClick, icon, altText, animated }) => {
+const CircleButton = ({ onClick, icon, altText, animated, inverted }) => {
 	return (
-		<div className="component-circle-button" onClick={onClick}>
+		<div
+			className={`component-circle-button ${
+				inverted ? "component-circle-button--inverted " : ""
+			}`}
+			onClick={onClick}
+		>
 			<img
 				className={`component-circle-button__image ${
 					animated ? "component-circle-button__image--animated" : ""
